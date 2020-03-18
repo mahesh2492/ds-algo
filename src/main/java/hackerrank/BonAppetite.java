b2a754b1052cf5f91542be0d149d69b1e95e1b36
mahesh2492 <mahesh.kndpl@gmail.com>
2020-03-18 17:07:57 +0530

    solved bon appetit problem

---
+++ src/main/java/hackerrank/BonAppetite.java
@@ -0,0 +1,49 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class BonAppetite {
+
+    static void bonAppetit(int[] bill, int k, int b) {
+        int deductedItem = bill[k];
+
+        int total = 0;
+        for (int bil : bill) {
+            total += bil;
+        }
+
+        int actual = (total - deductedItem) / 2;
+
+        if (actual == b) {
+            System.out.println("Bon Appetit");
+        } else {
+            System.out.println(abs(actual - b));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int t, sizeOfArray, k, charged;
+        Scanner sc = new Scanner(System.in);
+
+        t = sc.nextInt();
+
+        while (t-- > 0) {
+            sizeOfArray = sc.nextInt();
+            k = sc.nextInt();
+
+            int[] arr = new int[sizeOfArray];
+
+            for (int i = 0; i < sizeOfArray; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            charged = sc.nextInt();
+
+            bonAppetit(arr, k, charged);
+        }
+
+    }
+}
