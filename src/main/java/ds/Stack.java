2829bcfc4f20ae0fc2a3a4a3f92412ed7b3080d9
mahesh2492 <mahesh.kndpl@gmail.com>
2020-04-07 09:58:42 +0530

    implemented stack

---
+++ src/main/java/ds/Stack.java
@@ -0,0 +1,61 @@
+package ds;
+
+public class Stack {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    void push(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty(head)) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    boolean isEmpty(Node head) {
+        return head == null;
+    }
+
+    void pop() throws Exception {
+        if (isEmpty(head)) {
+            throw new Exception("Stack is empty");
+        }
+
+        head = head.next;
+    }
+
+    void display(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.display(head);
+        stack.pop();
+        stack.display(head);
+
+    }
+}
