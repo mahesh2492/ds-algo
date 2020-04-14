262a237554771ba497a65f952375639e1345df26
mahesh2492 <mahesh.kndpl@gmail.com>
2020-04-14 15:16:42 +0530

    removed unused imports from postfix evaluation clasd

--- src/main/java/ds/stack/PostfixEvaluation.java
+++ src/main/java/ds/stack/PostfixEvaluation.java
@@ -1,9 +1,8 @@
 package ds.stack;
-import javax.swing.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class PostfixEvaluation {
