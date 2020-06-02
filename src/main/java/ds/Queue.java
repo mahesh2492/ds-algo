8b53a05cc912e11e8d4bf826292c8d8f2df29496
mahesh2492 <mahesh.kndpl@gmail.com>
2020-06-02 10:40:12 +0530

    added front, queueSize and isEmpty method in Queue class

--- src/main/java/ds/Queue.java
+++ src/main/java/ds/Queue.java
@@ -44,21 +44,43 @@ public class Queue {
       head = head.next;
     }
 
+    int front() {
+        return head.data;
+    }
+
+    int queueSize() {
+      Node current = head;
+      int len = 0;
+      while (current != null) {
+          len++;
+          current = current.next;
+      }
+
+      return len;
+    }
+
+    boolean isEmptyQueue() {
+        return head == null;
+    }
 
     public static void main(String[] args) {
         Queue queue = new Queue();
 
+        System.out.println("is Queue empty?: "+ queue.isEmptyQueue());
         queue.insert(1);
         queue.insert(2);
         queue.insert(3);
         queue.insert(4);
 
+        System.out.println("Queue Front " + queue.front());
         queue.display();
 
         queue.delete();
         queue.display();
-
+        System.out.println("Queue size " + queue.queueSize());
         queue.delete();
         queue.display();
+
+
     }
 }
